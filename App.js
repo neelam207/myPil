aba2f5fcebdf5be101fc60232face00ae6aaf185
neelam207 <neelampant207@gmail.com>
2023-08-17 16:49:43 +0530

    redux toolkit template
    implemented redux toolkit template without middleware

---
+++ App.js
@@ -0,0 +1,12 @@
+import Home from './src/screens/Home';
+import { Provider } from "react-redux";
+import { store } from "./src/store";
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Home />
+    </Provider>
+  );
+}
+export default  App;
---
+++ src/screens/Home.js
@@ -0,0 +1,47 @@
+import { Text, StyleSheet, View, Button } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import { increment,decrement } from "../features/counter/counter";
+
+export default function Home() {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.counter.count);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.subtitle}>Current counter value: {count}</Text>
+      <Button
+        title="Increment"
+        onPress={() => {
+          dispatch(increment());
+        }}
+      />
+      <Text></Text>
+      <Button
+        title="Decrement"
+        onPress={() => {
+          dispatch(decrement());
+        }}
+      />
+      
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    marginBottom: 30,
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: "500",
+    marginBottom: 15,
+  },
+});
